95c8fa6a7fd4497ff8023bb5fae27225906adfa8
John Doe <gogs@fake.local>
2021-03-04 17:18:50 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/IStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    interface IStrings
+    {
+        string Convert();
+        string Cipher(string[] code);
+    }
+}
---
+++ ConsoleApp1/SimpleText.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class SimpleText:IStrings
+    {
+        string text;
+        static string[] codeTable =
+        {
+            "абвгдеёжзийкл",
+            "жзийклабвгдеё"
+        };
+
+        public SimpleText()
+        {
+            this.text = "Простой текст!";
+        }
+        public SimpleText(string text)
+        {
+            this.text = text;
+        }
+        public  string Text
+        {
+            get { return text; }
+        }
+        string IStrings.Cipher(string[] code)
+        {
+           string res="";
+           foreach(char sym in text)
+            {
+                int k = code[0].IndexOf(sym);
+                if (k > 0) res += code[1][k];
+                else res += sym.ToString();
+            }
+            return res;
+        }
+
+        string IStrings.Convert()
+        {
+            string res="";
+            foreach(char sym in text)
+            {
+                if (sym != ' ') res += sym.ToString();
+            }
+            res = res.ToLower();
+            return res;
+        }
+    }
+}
